d786c89c9a5e2eb339554ebfc1a09a0457275582
jihun1233 <leejihun6535@gmail.com>
2021-12-11 14:56:53 +0900

    [수정] 반응형 헤더 js
    - 강의영상 시청 후 수정
    
    - element.classList.toggle을 사용해 해당 클래스가 없으면 추가하고, 있으면 삭제하는 걸 한번에 해결.
    - 토글시 menuList, snsIcon에 클래스 activate를 추가,제거 하도록 구현

--- reactiveHeader/index.js
+++ reactiveHeader/index.js
@@ -1,15 +1,7 @@
-(function(window){
-  const $headerNav = document.querySelector(".headerNav");
-  const $menuToggle = document.querySelector(".menuToggle");
-  $menuToggle.addEventListener('click',()=>{
-    if(!$headerNav.classList.contains("headerNav_visible")){
-      $headerNav.classList.add("headerNav_visible");
-    }else{
-      $headerNav.classList.remove("headerNav_visible");
-    }
-    
-  
-  
-    
-  })
-})(window)
+const $menuList = document.querySelector(".menuList");
+const $menuToggleIcon = document.querySelector(".menuToggleIcon");
+const $snsIcon = document.querySelector('.snsIcon')
+$menuToggleIcon.addEventListener('click',()=>{
+  $menuList.classList.toggle('activate');
+  $snsIcon.classList.toggle('activate');
+})
