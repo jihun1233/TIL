66e7d9b6419b50e77b5f4814e4005ff0b049c6e9
jihun1233 <leejihun1233@gmail.com>
2022-03-01 22:03:35 +0900

    N퀸 문제 해결 - 그래프 dfs, 백트래킹

---
+++ algorithm/N-Queen.js
@@ -0,0 +1,16 @@
+// https://www.acmicpc.net/problem/9663
+// 백준 N퀸
+const rl = require('readline').createInterface({
+  input: process.stdin, output: process.stdout
+});
+
+
+
+
+rs.on('line',()=>{
+
+  rl.close();
+})
+.on('close',()=>{
+  process.exit();
+})
