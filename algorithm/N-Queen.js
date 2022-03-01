639cee3db6c1ca875310ef52185803935d1538e3
jihun1233 <leejihun1233@gmail.com>
2022-03-02 05:59:30 +0900

    N퀸 해결. 맞았습니다.
    백준 N퀸.
    
    col을 하나씩 추가해나가면서 모든 row를 검사해봐서 같은 앞선 col의 퀸 중에 같은 row에 있는 것, 같은 대각선에 있는 것(가로와 세로의 차이가 같은 경우)
    을 검사해서 없다면 bfs(col+1)로 진행하도록 함.

--- algorithm/N-Queen.js
+++ algorithm/N-Queen.js
@@ -5,12 +5,37 @@ const rl = require('readline').createInterface({
 });
 
 
+let n = 0;
+let temp = [];
+let count = 0;
+const bfs = (col) => {
+  if(col === n){
+    count++;
+    return;
+  }
+  for (let row = 0; row < n; row++) {
+    let check = true;
+    for (let i = 0; i < col; i++) {
+      temp[col] = row;
+      if(temp[i] === row || Math.abs(col - i) === Math.abs(row - temp[i])){
+        check = false;
+        break;
+      }
+    }
+    
+    if(check)bfs(col + 1);
+  }
 
-
-rs.on('line',()=>{
-
+};
+rl.on('line',(line)=>{
+  n = parseInt(line);
+  for (let index = 0; index < n; index++) {
+    temp[0] = index;
+    bfs(1);
+  }
   rl.close();
 })
 .on('close',()=>{
+  console.log(count);
   process.exit();
 })
